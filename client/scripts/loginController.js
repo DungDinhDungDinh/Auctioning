1d5308ead4fb59a2b99a57e19e2062a802cd0000
Noctis Lucis <noctislucispro@gmail.com>
2016-12-31 09:12:58 +0700

    edit login html, comtroller

--- client/scripts/loginController.js
+++ client/scripts/loginController.js
@@ -45,8 +45,19 @@ myapp.controller('loginController',  ['$scope', '$http', 'Data', '$location', '$
     $scope.password = '';
 
     $scope.login = function() {
-        if ($scope.username === '' && $scope.password === '') {
-            alert('Vui lập không để trống username và password');
+        if ($scope.username === '') {
+			$scope.errorText = " * Chưa nhập tên đăng nhập"
+            $scope.errorStyle = {"color":"red", "font-size":"12px", "display":"block"};
+			//angular.element('#inputName').css({'border-color': 'red',});
+			angular.element('#inputName').focus();
+            return;
+        }
+		
+		if ($scope.password === '') {
+			$scope.errorText = " * Chưa nhập mật khẩu"
+            $scope.errorStyle = {"color":"red", "font-size":"12px", "display":"block"};
+			//angular.element('#inputPassword').css({'border-color': 'red',});
+			angular.element('#inputPassword').focus();
             return;
         }
 
