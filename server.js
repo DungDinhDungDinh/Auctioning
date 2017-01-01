7cdce47202d24ef74abded9ce3b07b79a7371d7c
DungDinhDungDinh <channhoibong1995@gmail.com>
2017-01-01 16:00:12 +0700

    them api userauction-item

--- server.js
+++ server.js
@@ -507,8 +507,31 @@ apiRoutes.get('/search', function(req, res) {
 
 apiRoutes.get('/item_auctioning', function(req, res) {
     var _userID = req.query.userID;
-    var _itemID = req.query.itemID;
-   
+    Userauction.find({
+        userID: _userID
+    }).select().exec(function(err, userauctions) {
+        if (err) {
+            return res.status(404).send('Not found');
+            console.log('Failed!!');
+        } else {
+            //res.status(200).send(items);
+
+            //Tìm items của userauctions
+            userauctions.each(function(err, doc) {
+                Item.find({
+                    ID: userauction.itemID
+                }).select().exec(function(err, items) {
+                    if (err) {
+                        return res.status(404).send('Not found');
+                        console.log('Failed!!');
+                    } else {
+                        res.status(200).send(items);
+                        console.log(items);
+                    }
+                });
+            });     
+        }
+    });
 });
 
 apiRoutes.get('/item_follow', function(req, res) {
